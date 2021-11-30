9cf96929de447956fcfe247aaee0e7e110d1de5f
Martin Hoffmann <martin@nlnetlabs.nl>
2021-11-30 13:10:12 +0100

    Make util private for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-//! This is routecore, a Rust library for IP routing primitives.
+//! A library for IP routing primitives.
 
 pub mod addr;
 pub mod asn;
 pub mod bgp;
 pub mod bgpsec;
 pub mod record;
-pub mod util;
+
+
+//--- Private modules
+
+mod util;
