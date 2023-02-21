10d49afe9796a016bb8afff77b82d71ca05444f1
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-02-21 15:55:40 +0100

    Don't require the caller to depend on the right version of the octseq crate.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@
 
 pub mod addr;
 pub mod asn;
-#[cfg(feature = "bmp")]
-pub mod bmp;
 #[cfg(feature = "bgp")]
 pub mod bgp;
 pub mod bgpsec;
+#[cfg(feature = "bmp")]
+pub mod bmp;
 #[cfg(feature = "bgp")]
 pub mod flowspec;
 pub mod record;
 
+#[cfg(feature = "bmp")]
+pub use octseq::Octets;
 
 //--- Private modules
 
