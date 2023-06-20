31e870fee8cfb12bca32f67c191d22b57b79945b
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-06-20 22:33:31 +0000

    Derive Serialize for Aggregator.

--- src/bgp/message/update.rs
+++ src/bgp/message/update.rs
@@ -1266,6 +1266,7 @@ impl<'a, Ref: Octets> IntoIterator for PathAttributes<'a, Ref> {
 //--- Aggregator -------------------------------------------------------------
 /// Path Attribute (7).
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 pub struct Aggregator {
     asn: Asn,
     speaker: Ipv4Addr,
