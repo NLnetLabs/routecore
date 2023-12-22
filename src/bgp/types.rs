974270c7e2da954ef2306efdb113d6cf29e6e6f0
Jasper den Hertog <jasper@plainspace.com>
2023-12-22 12:10:02 +0100

    add From<LocalPref>

--- src/bgp/types.rs
+++ src/bgp/types.rs
@@ -283,6 +283,12 @@ impl std::fmt::Display for MultiExitDisc {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct LocalPref(pub u32);
 
+impl From<LocalPref> for u32 {
+    fn from(value: LocalPref) -> Self {
+        value.0
+    }
+}
+
 impl std::fmt::Display for LocalPref {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
